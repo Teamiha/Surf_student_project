1773a165fd4a4f344c97a6948dd4ec481c3ab709
Mikhail Svetlov <jegnum@gmail.com>
2023-02-06 19:09:36 +0400

    Initial Commit

---
+++ Surf_student_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Surf_student_project
+//
+//  Created by Михаил Светлов on 06.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
